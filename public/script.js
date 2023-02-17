c26d6bbca18dce28833a9cc1723ba0f1f97e87c2
Frank Kelly <frankintech00@gmail.com>
2023-02-17 00:26:11 +0000

    Initial commit

---
+++ public/script.js
@@ -0,0 +1,8 @@
+//todo Run build
+
+//todo change title in package.json
+//todo change description in package.json
+//todo change title in index.html
+//todo change description in index.html
+//todo change title in manifest.json
+//todo change description in manifest.json
