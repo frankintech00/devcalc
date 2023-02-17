546ed4f50abd7960386faf2d58c94740a55eeb38
Frank Kelly <frankintech00@gmail.com>
2023-02-17 00:30:13 +0000

    chnaged project details and ran build

--- public/script.js
+++
@@ -1,8 +0,0 @@
-//todo Run build
-
-//todo change title in package.json
-//todo change description in package.json
-//todo change title in index.html
-//todo change description in index.html
-//todo change title in manifest.json
-//todo change description in manifest.json
